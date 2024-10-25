215b8d4a3faa4c1fefbd7ea30a23befe5b17dcc4
GustavFasa <gustavkarlholm@live.se>
2024-10-25 09:18:12 +0200

    flyttade runt entiteter

--- LogicLayer/Station.cs
+++ LogicLayer/Station.cs
@@ -8,5 +8,23 @@ namespace BuissnessEnteties
 {
     internal class Station
     {
-    }
+        public class StationData //Definierar vilken data som finns kring de olika programmen, om man tex skulle behöva lägga till/ta bort ett program.
+        {
+            public string Adress { get; private set; }
+            public int AntalFordon { get; private set; }
+            public string FordonStatus { get; private set; }
+            public int MaxKapacitet { get; private set; }
+
+            public StationData(string adress, int antalFordon, string fordonStatus, int maxKapacitet)
+            {
+                Adress = adress;
+                AntalFordon = antalFordon;
+                FordonStatus = fordonStatus;
+                MaxKapacitet = maxKapacitet;
+            }
+            public bool FinnsPlats()
+            {
+                return AntalFordon < MaxKapacitet;
+            }
+        }
 }
