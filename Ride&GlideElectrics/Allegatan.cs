e75b2d0114281f63c96a30862cdd08cc476d3ca7
exiiee98 <swedishrally76@gmail.com>
2024-10-27 08:33:18 +0100

    1234567

---
+++ Ride&GlideElectrics/Allegatan.cs
@@ -0,0 +1,57 @@
+﻿using Servicelager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Servicelager;
+using BusinessEntities;
+
+namespace Presentationslager
+{
+    public partial class Allegatan : Form
+    {
+        private List<Fordon> _fordonLista;
+
+        public Allegatan()
+        {
+            InitializeComponent();
+            InitializeData();
+        }
+
+
+        private void InitializeData() // kopplar listan till datagrid
+        {
+            // Initialize the list from in-memory data source
+            // Initialize the list from in-memory data source
+            _fordonLista = FordonRepository.GetAllFordon();
+
+
+            // Filter the list to include only vehicles where Position contains "Allegatan"
+            var filteredFordonLista = _fordonLista.Where(f => f.Position.Contains("Allégatan 10")).ToList();
+
+            // Set the filtered list as the DataSource for the DataGridView
+            dataGridView1.DataSource = filteredFordonLista;
+
+            // Set header text for the columns
+            dataGridView1.Columns["FordonsID"].HeaderText = "ID";
+            dataGridView1.Columns["Position"].HeaderText = "Station";
+            dataGridView1.Columns["Status"].HeaderText = "Status";
+            dataGridView1.Columns["FordonsTyp"].HeaderText = "Typ utav fordon";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
--- Ride&GlideElectrics/HyrFordon.cs
+++ Ride&GlideElectrics/HyrFordon.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Servicelager;
 using BusinessEntities;
+using Presentationslager;
 
 namespace Ride_GlideElectrics
 {
@@ -26,6 +27,35 @@ namespace Ride_GlideElectrics
             usermenu.Show();
             this.Close();
 
+        }
+
+        private void btn_Allégatan_Click(object sender, EventArgs e)
+        {
+            Allegatan allegatan = new Allegatan();
+            allegatan.Show();
+            this.Hide();
+        }
+
+        private void btn_Stationsgatan_Click(object sender, EventArgs e)
+        {
+            Stationsgatan stationsgatan = new Stationsgatan();
+            stationsgatan.Show();
+            this.Hide();
+        }
+
+        private void btn_Fredriksbergsgatan_Click(object sender, EventArgs e)
+        {
+            //Fredriksbergsgatan fredriksbergsgatan = new Fredriksbergsgatan();
+           //fredriksbergsgatan.Show();
+            //this.Hide();
+
+        }
+
+        private void btn_Solrosvägen_Click(object sender, EventArgs e)
+        {
+           Solrosvagen solrosvagen = new Solrosvagen();
+           solrosvagen.Show();
+           this.Hide();
         }
     }
 }
