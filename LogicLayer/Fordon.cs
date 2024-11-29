9b8d89db42049cac91e836acfced1d84ace8ecea
exiiee98 <swedishrally76@gmail.com>
2024-11-29 09:32:28 +0100

    6126126126

--- LogicLayer/Fordon.cs
+++ LogicLayer/Fordon.cs
@@ -14,18 +14,18 @@ namespace BusinessEntities
             public int FordonsID { get; private set; }
             public string Position { get; set; }
 
-            public int Batteriniva { get; set; }
+            public int Batterinivå { get; set; }
             public string Status { get; set; }
             public string FordonsTyp { get; set; }
         [Browsable(false)] public StationData StationData { get; set; } // 0..1 - Ett fordon kan finnas på en station och Datagridview har ingen åtkomst till att visa den
         public List<UthyrningsData> Uthyrningar { get; set; } = new List<UthyrningsData>(); // 1..*
 
         // Konstruktor
-        public Fordon(int fordonsID, string position, int batteriniva, string status, string fordonsTyp)
+        public Fordon(int fordonsID, string position, int batterinivå, string status, string fordonsTyp)
         {
             FordonsID = fordonsID;
             Position = position;
-            Batteriniva = batteriniva;
+            Batterinivå = batterinivå;
             Status = status;
             FordonsTyp = fordonsTyp;
         }
